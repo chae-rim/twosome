2bc57a6f01992a039c06e3df25136e1d1379e101
chae-rim <108618066+chae-rim@users.noreply.github.com>
2022-12-12 12:17:57 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,64 @@
+//네비
+
+$(document).ready(function () {
+    $('#header .navbar').hide();
+    $('.nav li').click(function () {
+        $('#header .navbar').toggle();
+    });
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+//메인이미지
+
+$(document).ready(function () {
+    var current = 0
+    var setIntervalId;
+
+
+    timer();
+    function timer() {
+        setIntervalId = setInterval(function () {
+            var n = current + 1;
+            if (n == 2) {
+                n = 0;
+            }
+            move(n);
+        }, 5000)
+
+    }
+    function move(n) {
+        var currentEl = $('.main_img li').eq(current);
+        var nextEl = $('.main_img li').eq(n);
+
+
+        currentEl.css({ left: '0%' }).animate({ left: '-100%' });
+        nextEl.css({ left: '100%' }).animate({ left: '0%' });
+
+
+        current = n;
+    }
+});
+
+
+
+
+
+
+//시그니처
+$(document).ready(function () {
+    $('#contents .menu_img');
+    $('.menu_img li').click(function () {
+        $('#contents .menu_img').toggle();
+    });
+})
